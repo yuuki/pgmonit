f70e5fd98939e22731f47628e31f96f9804f8842
y_uuki <yuki.tsubo@gmail.com>
2014-11-11 23:50:20 +0900

    Add simple http handler

--- pgmonit.go
+++ pgmonit.go
@@ -8,23 +8,23 @@ import (
 )
 
 type Options struct {
-    Version bool `short:"v" long:"version" description:"Show version"`
+	Version bool `short:"v" long:"version" description:"Show version"`
 }
 
 var opts Options
 
 func main() {
 	parser := flags.NewParser(&opts, flags.Default)
-	args, err := parser.Parse()
+	_, err := parser.Parse()
 	if err != nil {
 		parser.WriteHelp(os.Stdout)
 		os.Exit(1)
 	}
 
 	if opts.Version {
-		fmt.Printf("version %s\n", Version)
+		fmt.Printf("pgmonit v%s\n", Version)
 		os.Exit(0)
 	}
 
-	fmt.Printf("args %s\n", args)
+	AppUp()
 }
